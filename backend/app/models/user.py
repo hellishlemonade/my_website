a6710ee7a3f252916e7f083b1df87d4e4400df56
Dmitriy Lyapin <d.lyapin29@yandex.ru>
2026-04-07 20:39:00 +0300

    database: create user model

---
+++ backend/app/models/user.py
@@ -0,0 +1,41 @@
+from enum import Enum
+from typing import List, Optional
+
+from sqlalchemy import Enum as SQLEnum
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from models.base import Base
+
+
+class UserRole(Enum):
+    """
+    Enum для добавления статусов пользователей
+    """
+
+    ADMIN = "admin"
+    CUSTOMER = "customer"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    first_name: Mapped[Optional[str]] = mapped_column(String(30))
+    last_name: Mapped[Optional[str]] = mapped_column(String(30))
+    username: Mapped[str] = mapped_column(String(20), unique=True)
+    _password_hash: Mapped[str] = mapped_column(String(255))
+    role: Mapped[UserRole] = mapped_column(
+        SQLEnum(UserRole, name="user_role_enum"),
+        default=UserRole.CUSTOMER,
+        native_enum=False,
+        nullable=True,
+    )
+
+    def set_password(self, password):
+        pass
+
+    def verify_password(self, password):
+        pass
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
