283e96636c65914a59ff10022b367d7b6fbf145d
Dmitriy Lyapin <d.lyapin29@yandex.ru>
2026-04-09 20:52:57 +0300

    database: add methods for hash password and fix __init__ for pydantic model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,10 +1,11 @@
 from enum import Enum
-from typing import Optional
+from typing import Any, Optional
 
 from sqlalchemy import Enum as SQLEnum
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from app.core.security import hash_pwd, verify_pwd
 from app.models.base import Base
 
 
@@ -40,11 +41,19 @@ class User(Base):
         back_populates="liked_by", secondary="post_likes"
     )
 
-    def set_password(self, password):
-        pass
+    def __init__(self, **kwargs: Any):
+        if "password" in kwargs:
+            kwargs["_password_hash"] = hash_pwd(kwargs.pop("password"))
+        super().__init__(**kwargs)
 
-    def verify_password(self, password):
-        pass
+    def set_password(self, password: str):
+        return hash_pwd(password)
+
+    def verify_password(self, password: str, hash_password: str):
+        verified, new_hash = verify_pwd(password, hash_password)
+        if new_hash:
+            self._password_hash = new_hash
+        return verified
 
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
