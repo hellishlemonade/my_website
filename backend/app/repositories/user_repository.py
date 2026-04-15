1eb246c17eb0b17d236056dd63157eb8f89dfad7
Dmitriy Lyapin <d.lyapin29@yandex.ru>
2026-04-15 23:00:04 +0300

    repositories: add new methods for id search and change return for add_one

--- backend/app/repositories/user_repository.py
+++ backend/app/repositories/user_repository.py
@@ -1,3 +1,5 @@
+from typing import Sequence
+
 from sqlalchemy import select
 
 from app.database.session import session
@@ -16,18 +18,22 @@ class UserRepository:
               Возвращает id созданного пользователя.
 
     -get_by_username: Используется для поиска пользователя
-                      по его username полюи возвращает объект
-                      пользователя или None. 
+                      по его username полю и возвращает объект
+                      пользователя или None.
+    
+    -get_by_id: Используется для поиска пользователя
+                по его id полю и возвращает объект пользователя
+                или None.
     """
     @classmethod
-    async def add_one(cls, data: SUserCreate) -> int:
+    async def add_one(cls, data: SUserCreate) -> User:
         async with session() as new_session:
             user_dict = data.model_dump()
             user_dict["password"] = user_dict.pop("password").get_secret_value()
             user = User(**user_dict)
             new_session.add(user)
             await new_session.commit()
-            return user.id
+            return user
 
     @classmethod
     async def get_by_username(cls, username: str) -> User | None:
@@ -35,3 +41,21 @@ class UserRepository:
             query = select(User).where(User.username == username)
             result = await new_session.execute(query)
             return result.scalar_one_or_none()
+
+    @classmethod
+    async def get_by_id(cls, id: int) -> User | None:
+        async with session() as new_session:
+            query = select(User).where(User.id == id)
+            result = await new_session.execute(query)
+            user_model = result.scalar_one_or_none()
+            return user_model
+
+    @classmethod
+    async def get_users(cls) -> Sequence[User]:
+        async with session() as new_session:
+            query = select(User)
+            result = await new_session.execute(query)
+            user_models = result.scalars().all()
+            return user_models
+    
+
